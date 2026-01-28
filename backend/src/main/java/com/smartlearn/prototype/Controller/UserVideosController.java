1fa31348a11a0eaebe12f94a9fc50041a77166a5
kamogelo <kamozozobaete@gmail.com>
2026-01-28 19:07:17 +0200

    user video feature added --> to do

--- backend/src/main/java/com/smartlearn/prototype/Controller/UserVideosController.java
+++ backend/src/main/java/com/smartlearn/prototype/Controller/UserVideosController.java
@@ -2,8 +2,22 @@ package com.smartlearn.prototype.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.transaction.Transactional;
 @RestController
-@RequestMapping("/api/user/videos")
+@RequestMapping("/api/user/video")
 public class UserVideosController {
-    
+    @PostMapping
+    public String postVideo(){
+        return "video created";
+    }
+    @Transactional
+    @DeleteMapping("/{videoId}")
+    public String DeleteVideo(){
+        return "video deleted";
+    }
+    @GetMapping("/{videoId}")
+    public String getVideo(){
+        return "video recieved";
+    }
 }
--- backend/src/main/java/com/smartlearn/prototype/model/UserVideo.java
+++ backend/src/main/java/com/smartlearn/prototype/model/UserVideo.java
@@ -2,26 +2,38 @@ package com.smartlearn.prototype.model;
 
 
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import static jakarta.persistence.FetchType.LAZY;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name="video")
-public class Video{
+@Table(name="videos")
+public class UserVideo{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     private String title;
+    @Column(nullable = false)
     private String url;
     private String resolution;
+    @Column(name = "upload_date", nullable = false, updatable = false)
     private LocalDateTime uploadDate;
-    @OneToMany(mappedBy="featuredVideo")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "featured_video_id", referencedColumnName = "id",nullable=false)
+    @JsonBackReference 
     private User user;
 
 }
