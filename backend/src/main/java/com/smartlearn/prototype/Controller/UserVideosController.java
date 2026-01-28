84236feda6c5d620b1986541db635c6e687a4067
kamogelo <kamozozobaete@gmail.com>
2026-01-28 17:57:05 +0200

    video controller intitialized

---
+++ backend/src/main/java/com/smartlearn/prototype/Controller/UserVideosController.java
@@ -0,0 +1,9 @@
+package com.smartlearn.prototype.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/api/user/videos")
+public class UserVideosController {
+    
+}
