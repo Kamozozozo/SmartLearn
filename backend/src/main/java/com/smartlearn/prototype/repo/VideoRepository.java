6e276884ab70f97b8aaca016f87d8d13464cd001
kamogelo <kamozozobaete@gmail.com>
2026-01-29 21:12:21 +0200

    videoRepo initialized

---
+++ backend/src/main/java/com/smartlearn/prototype/repo/VideoRepository.java
@@ -0,0 +1,11 @@
+package com.smartlearn.prototype.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.smartlearn.prototype.model.UserVideo;
+
+@Repository
+public  interface  VideoRepository extends JpaRepository<UserVideo,String> {
+    
+}
