9ed7033415b8f0a1829c643578fe3f9054827003
kamogelo <kamozozobaete@gmail.com>
2026-01-31 16:18:49 +0200

    higlight for the landing page done ony API call left

--- frontend/src/landingpage/Highlights.jsx
+++ frontend/src/landingpage/Highlights.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 const Highlights = () => {
   return (
-    <div>Highlights</div>
-  )
-}
+    <div className="py-4">
+      <div className="flex overflow-x-auto space-x-4 px-4 scrollbar-hid no-scrollbar">
+      
+        <div className="flex-shrink-0 w-[33.33vw] h-60 bg-red-50 rounded-xl shadow-lg flex items-center justify-center">
+          div1
+        </div>
+        <div className="flex-shrink-0 w-[33.33vw] h-60 bg-blue-50 rounded-xl shadow-lg flex items-center justify-center">
+          div2
+        </div>
+        <div className="flex-shrink-0 w-[33.33vw] h-60 bg-green-50 rounded-xl shadow-lg flex items-center justify-center">
+          div3
+        </div>
+        <div className="flex-shrink-0 w-[33.33vw] h-60 bg-yellow-50 rounded-xl shadow-lg flex items-center justify-center">
+          div4
+        </div>
+        <div className="flex-shrink-0 w-[33.33vw] h-60 bg-purple-50 rounded-xl shadow-lg flex items-center justify-center">
+          div5
+        </div>
+        <div className="flex-shrink-0 w-[33.33vw] h-60 bg-pink-50 rounded-xl shadow-lg flex items-center justify-center">
+          div6
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Highlights
+export default Highlights;
