1c28b8c655dfb5a8c399430d8f761056b59d2483
kamogelo <kamozozobaete@gmail.com>
2026-01-31 15:38:49 +0200

    body font halfway

--- frontend/src/landingpage/BodyFront.jsx
+++ frontend/src/landingpage/BodyFront.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import Highlights from './Highlights';
 const BodyFront = () => {
   return (
     <>
-    
-    <div className="flex items-center justify-center  mt-30 px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-      <div className='grid grid-flex-row gap-10'>
-        <div className='font-bold text-6xl md:text-8xl lg:text-9xl'>
+    <div className="pt-40 w-screen overflow-hidden border-b border-gray-300 bg-white py-8">
+    <div className="grid gap-10">      
+      <div className="font-bold text-6xl md:text-8xl lg:text-9xl text-center">
         startuphub
       </div>
-      <div className=' px-9 text-gray-500 text-2xl md:text-4xl lg:text-5xl '>
-        allow your idea to shine by getting ivestors and  find co-founders like you to collaborate
+      <div className="overflow-hidden w-screen">
+        <div className="text-gray-500 text-2xl md:text-4xl lg:text-5xl whitespace-nowrap w-max animate-marquee">
+          Allow your idea to shine by getting investors and find co-founders like you to collaborate.
+          &nbsp;&nbsp;&nbsp;
+          Allow your idea to shine by getting investors and find co-founders like you to collaborate.
+        </div>
       </div>
-      </div>
+
     </div>
+  </div>
+  <Highlights/>
+
     </>
   )
 }
