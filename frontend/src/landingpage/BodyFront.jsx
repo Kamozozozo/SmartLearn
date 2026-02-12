80cb2e499dff88e02bd40930856b7f4e0c12d00e
kamogelo <kamozozobaete@gmail.com>
2026-02-12 18:30:59 +0200

    body front added

--- frontend/src/landingpage/BodyFront.jsx
+++ frontend/src/landingpage/BodyFront.jsx
@@ -5,8 +5,12 @@ const BodyFront = () => {
     <>
     <div className="pt-40 w-screen overflow-hidden border-b border-gray-300 bg-gray py-8">
     <div className="grid gap-10">      
-      <div className="font-bold text-6xl md:text-8xl lg:text-9xl text-center text-primary font-serif font-bold">
-        startuphub
+      <div className=" text-center">
+        <span className='font-bold text-3xl md:text-5xl lg:text-6xl text-foreground font-serif font-bold'>WELCOME TO</span>
+        <br></br>
+        <span className='font-bold text-6xl md:text-8xl lg:text-9xl text-primary font-serif font-bold'>
+        STARTUP HUB
+        </span>
       </div>
       <div className="overflow-hidden w-screen">
         <div className="text-gray-500 text-1xl md:text-4xl lg:text-5xl whitespace-nowrap w-max animate-marquee">
