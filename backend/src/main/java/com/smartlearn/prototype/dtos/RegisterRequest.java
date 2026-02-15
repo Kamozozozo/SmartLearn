f40ec9ca2c9a93413bf32d0842d4979371f5ae00
kamogelo <kamozozobaete@gmail.com>
2026-02-15 04:29:41 +0200

    register dto updated

--- backend/src/main/java/com/smartlearn/prototype/dtos/RegisterRequest.java
+++ backend/src/main/java/com/smartlearn/prototype/dtos/RegisterRequest.java
@@ -1,7 +1,12 @@
 
 package com.smartlearn.prototype.dtos;
 
+import com.smartlearn.prototype.model.UserRole;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
+import jakarta.persistence.EnumType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -11,6 +16,9 @@ public class RegisterRequest{
     @NotBlank(message="email is required")
     @Email(message="invalid email request")
     private String email;
+    @NotBlank(message="role is required")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
     @NotBlank(message="password is required")
     @Size(min=6,message="password must be altleast 6 characters long")
     private String password;
