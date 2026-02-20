91a972274ac4b5b6153ecc0d93b4cb3424c3a55a
kamogelo <kamozozobaete@gmail.com>
2026-02-20 03:18:16 +0200

    job dtos updated

--- backend/src/main/java/com/smartlearn/prototype/dtos/JobRequest.java
+++ backend/src/main/java/com/smartlearn/prototype/dtos/JobRequest.java
@@ -1,5 +1,20 @@
 package com.smartlearn.prototype.dtos;
 
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class JobRequest {
+    @NotBlank(message="descriptions is required")
+    private String descriptions;
+    @NotBlank(message="company is required")
+    private String company;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
     
 }
--- backend/src/main/java/com/smartlearn/prototype/dtos/JobResponse.java
+++ backend/src/main/java/com/smartlearn/prototype/dtos/JobResponse.java
@@ -1,5 +1,14 @@
 package com.smartlearn.prototype.dtos;
 
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.Builder;
+@Data
+@Builder
 public class JobResponse {
-    
+    private String id;
+    private String descriptions;
+    private String company;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
