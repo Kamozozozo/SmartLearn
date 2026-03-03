951f0dd1d869ac54f61697fbb9651720ac40d81b
kamogelo <kamozozobaete@gmail.com>
2026-03-03 03:49:43 +0200

    JobRepository updated for the methon ExistBYId

--- backend/src/main/java/com/smartlearn/prototype/repo/JobRepository.java
+++ backend/src/main/java/com/smartlearn/prototype/repo/JobRepository.java
@@ -2,9 +2,13 @@ package com.smartlearn.prototype.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.smartlearn.prototype.model.Jobs;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface JobRepository extends JpaRepository<Jobs, String> {
+
+    List<Jobs> findAll();
 
     
 }
